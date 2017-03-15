24af2657bbeb561a1e1fd2f874ff85497c4604b5
Joshua Parsels <joshparsels@gmail.com>
2017-03-15 16:31:54 -0700

    Dominos

---
+++ Dominos/Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            var xu = Input.GetAxis("Horizontal") * Time.deltaTime * 700.0f;
+            var zu = Input.GetAxis("Vertical") * Time.deltaTime * 100.0f;
+			// print(Input.Get);
+			print(Input.GetAxis("Vertical"));
+
+            transform.Rotate(0, xu, 0);
+            transform.Translate(0, 0, zu);
+        }
+        else
+        {
+
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        }
+    }
+}
---
+++ Dominos/Assets/procedural_generate_dominos.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class procedural_generate_dominos : MonoBehaviour {
+	public GameObject to_replicate;
+	public int how_many;
+	
+
+	// Use this for initialization
+	void Start () {
+		Vector3 originalPos = gameObject.transform.position;
+		for(var i = 1; i <= how_many; i++){
+			Instantiate(to_replicate,(originalPos + new Vector3(0, 0 ,i * 2.2f)), Quaternion.identity);
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
